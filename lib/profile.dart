94e8f3037a5a9020ce8bc297b8d9979073acdc27
Abeljo <abeljo86@gmail.com>
2022-01-26 19:49:54 +0300

    added circular avatar icon

--- lib/profile.dart
+++ lib/profile.dart
@@ -3,14 +3,36 @@ import 'package:flutter/material.dart';
 class profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: Text('Profile'),
-          centerTitle: true,
-          backgroundColor: Colors.blue,
-        ),
-        body: Center(
-          child: Text('Profile',
-              style: TextStyle(color: Colors.blue, fontSize: 60)),
-        ),
-      );
+      appBar: AppBar(
+        title: Text('Profile'),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text('Profile',
+                style: TextStyle(color: Colors.blue, fontSize: 60)),
+          ),
+          SafeArea(
+              child: Padding(
+            padding: EdgeInsets.only(top: 10),
+          )),
+          CircleAvatar(
+            backgroundColor: Color(0xffE6E6E6),
+            radius: 70,
+            child: Icon(
+              Icons.person,
+              color: Colors.blue,
+              size: 70,
+            ),
+          ),
+          Text(
+            'Abel Jo',
+            style: TextStyle(color: Colors.black26, fontSize: 30),
+          ),
+        ],
+      ));
 }
